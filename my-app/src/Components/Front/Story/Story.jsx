a6c73f34135736cf9e14069eee2a8d1e96866105
julijajarmale <julija.jarmale@gmail.com>
2022-08-05 13:08:17 +0300

    Create Story and List

---
+++ my-app/src/Components/Front/Story/Story.jsx
@@ -0,0 +1,23 @@
+function Story({ story }) {
+  return (
+    <li className="list-item">
+      <div className="content">
+        <span className="item">{story.title}</span>
+
+        <span className="item">{story.sum}</span>
+        <p style={{ color: story.approved ? "green" : "red" }}>
+          Approved: {story.approved ? "Yes" : "No"}
+        </p>
+      </div>
+      <div className="item herbas">
+        {story.picture ? (
+          <div className="photo-bin">
+            <img src={story.picture} alt={story.title} />
+          </div>
+        ) : null}
+      </div>
+    </li>
+  );
+}
+
+export default Story;
