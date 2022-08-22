ba744b7a9b31b8997c23cf0d21826b827485aab5
julijajarmale <julija.jarmale@gmail.com>
2022-08-22 10:40:58 +0300

    registration

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,6 +7,7 @@ import {
     Route,
     Navigate,
     useNavigate,
+    Link,
   
 } from "react-router-dom";
 import Front from './Components/Front/Front';
@@ -22,6 +23,7 @@ function App() {
     <Routes> 
         <Route path="/" element={<Front show="public"/>} />
         <Route path="/story" element={<Front show="story"/>} />
+        <Route path='/register' element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/logout" element={<LogoutPage />} />
         <Route path="/admin" element={<RequireAuth role="admin"><Back show="admin" /></RequireAuth>} />
@@ -83,6 +85,72 @@ function RequireAuth({ children, role }) {
         </div>
       );
     }
+//////////////////REGISTER PAGE////////////
+function RegisterPage() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+  const [email, setEmail] = useState('');
+
+  const doRegister = () => {
+    axios.post('http://localhost:3003/register', { user, pass, email }).then((res) => {
+      if ('ok' === res.data.msg) {
+        login(res.data.key);
+        navigate('/', { replace: true });
+      }
+    });
+  };
+  return (
+    <>
+      <div className="container login-container">
+      <div className="row">
+        <div className="col-12 login-form">
+        <h2 className='heading'>CREATE ACCOUNT</h2>
+        <div>
+              Name:{" "}
+              <input
+                className="input"
+                type="text"
+                value={user}
+                onChange={(e) => setUser(e.target.value)}
+              ></input>
+            </div>
+        <div>
+          Email:{' '}
+          <input
+          className="input"
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          Password:
+          <input
+          className="input"
+            type='password'
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
+        </div>
+        <button
+        className="btn buttons"
+          onClick={doRegister}>
+          Register
+        </button>
+        <Link
+          to='/login'
+          className=''>
+          Login
+        </Link>
+      </div>
+      </div>
+      </div>
+    </>
+  );
+}
+
   function LogoutPage() {
     useEffect(() => logout(), []);
     return (
